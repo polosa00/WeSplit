2f6434f5452cb63fa10e8ab15e9367a089d15b5e
polosa00 <i@apolochanin.ru>
2023-10-24 00:12:18 +0300

    Challenge Day

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
@@ -68,10 +69,72 @@ struct ContentView: View {
         }
     }
 }
+struct LongConverter: View {
+    @State private var longMeters = 0.0
+    @FocusState private var isFocusedLong: Bool
+    
+    var footCalculateValue: Double {
+        longMeters * 3.28
+    }
+    
+    var seaMilesCalculateValue: Double {
+        longMeters * 0.00053
+    }
+    
+    var milesCalculateValue: Double {
+        longMeters * 0.00062
+    }
+    
+    var kilometersCalculateValue: Double {
+        longMeters * 0.001
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Enter Meters") {
+                    TextField("Enter meters", value: $longMeters, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocusedLong)
+                }
+                
+                Section("In Kilometers") {
+                    Text(kilometersCalculateValue, format: .number)
+                }
+                
+                Section("In Sea Miles") {
+                    Text(seaMilesCalculateValue, format: .number)
+                }
+                
+                Section("In Miles") {
+                    Text(milesCalculateValue, format: .number)
+                }
+                
+    
+                Section("In Foots") {
+                    Text(footCalculateValue, format: .number)
+                }
+                
+            }
+            .navigationTitle("LongConverter")
+            .toolbar {
+                if isFocusedLong {
+                    Button {
+                        isFocusedLong = false
+                    } label: {
+                        Image(systemName: "checkmark")
+                    }
+
+                }
+            }
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+//        ContentView()
+        LongConverter()
     }
 }
  
--- WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplitApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct WeSplitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LongConverter()
         }
     }
 }
