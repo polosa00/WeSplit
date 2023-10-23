65fffefa14eeb86dcd8c9c50cc52abf8785ab571
polosa00 <i@apolochanin.ru>
2023-10-23 18:43:23 +0300

    Create first steps in App

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [ 10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Ammount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BYN"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of peoples", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) peoples")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BYN"))
+                }
+            }
+            .navigationTitle("WeSplit")
         }
-        .padding()
     }
 }
 
