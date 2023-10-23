da42c6f07f892b1e8e4ebe6bbb0eb7164f5fd568
polosa00 <i@apolochanin.ru>
2023-10-23 23:14:29 +0300

    We Split is ready

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var checkAmount = 0
+    @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var isFocused: Bool
     
     let tipPercentages = [ 10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipAmount = checkAmount / 100 * tipSelection
+        let totalAmount = tipAmount + checkAmount
+        let amountPerPeople = totalAmount / peopleCount
+        
+        return amountPerPeople
+    }
     
     var body: some View {
         NavigationStack {
@@ -20,6 +33,7 @@ struct ContentView: View {
                 Section {
                     TextField("Ammount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BYN"))
                         .keyboardType(.decimalPad)
+                        .focused($isFocused)
                     Picker("Number of peoples", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) peoples")
@@ -28,11 +42,29 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
                 
+                
+                Section("What tip would you like?") {
+                    Picker("", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BYN"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "BYN"))
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if isFocused {
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
         }
     }
 }
@@ -42,3 +74,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
